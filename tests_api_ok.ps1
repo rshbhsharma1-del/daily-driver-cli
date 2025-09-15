7d790021fb8af367e9f40479f5bfa00282354c68
Rishabh <rshbh.sharma1@gmail.com>
2025-09-15 17:03:27 +0530

    Day 7 Step-3a: add good-request API test

---
+++ tests_api_ok.ps1
@@ -0,0 +1,4 @@
+﻿$okBody = @{ user_id = 1; timeout = 5 } | ConvertTo-Json
+$res = Invoke-RestMethod -Method Post -Uri http://127.0.0.1:8000/process -Body $okBody -ContentType 'application/json'
+if ($res.status -ne 'ok') { Write-Error "Expected status=ok. Got: $($res | ConvertTo-Json -Depth 5)"; exit 1 }
+"PASS: good request"
